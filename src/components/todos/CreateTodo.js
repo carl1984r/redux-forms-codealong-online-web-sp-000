b044fbfcec6616b83fab5484368ce1a5e4304112
Bry Cox <carl.cc@gmail.com>
2020-04-12 20:05:24 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class CreateTodo extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state);
+  };
+  
   render() {
     return(
       <div>
