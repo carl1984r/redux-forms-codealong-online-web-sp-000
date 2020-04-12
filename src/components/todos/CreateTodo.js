0a0d433e0a97a9ddd6ac01f0bcd4531938a93d0a
Bry Cox <carl.cc@gmail.com>
2020-04-12 20:08:02 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -34,4 +34,12 @@ class CreateTodo extends Component {
   }
 }
 
-export default CreateTodo;
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+  };
+};
+
+
+
+export default connect(null, mapDispatchToProps)(CreateTodo);
